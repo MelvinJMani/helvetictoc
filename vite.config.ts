37590b24cbf431e7feaa3c34bffffb6955117883
Melvin Joseph Mani <melvinjmani@gmail.com>
2024-10-14 14:39:44 +0530

    Fix for relative path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    // Set the base URL conditionally based on the mode (development or production)
+    base: mode === 'production' ? '/repository-name/' : '/',  // Change 'repository-name' to your GitHub repo name
+  };
+});
